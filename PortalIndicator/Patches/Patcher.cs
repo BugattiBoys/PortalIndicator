95613f2a6815d02d2876a804e3c9b02e45030617
Robert Wittman <robkwittman@gmail.com>
2024-02-11 21:17:28 -0500

    Fix build for PortalIndicator

---
+++ PortalIndicator/Patches/Patcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using PortalIndicator.Patches;
+
+namespace PortalIndicator.Patches
+{
+    internal static class Patcher
+    {
+        private static readonly Harmony patcher = new Harmony(PortalIndicator.PluginGUID + ".harmony");
+        public static void Patch()
+        {
+            patcher.PatchAll(typeof(Game_Start));
+        }
+
+        public static void Unpatch() => patcher?.UnpatchSelf();
+    }
+}
--- PortalIndicator/PortalIndicator.cs
+++ PortalIndicator/PortalIndicator.cs
@@ -10,7 +10,7 @@ namespace PortalIndicator
     //[NetworkCompatibility(CompatibilityLevel.EveryoneMustHaveMod, VersionStrictness.Minor)]
     internal class PortalIndicator : BaseUnityPlugin
     {
-        public const string PluginGUID = "com.jotunn.jotunnmodstub";
+        public const string PluginGUID = "BugattiBoys.Valheim.PortalIndicator";
         public const string PluginName = "PortalIndicator";
         public const string PluginVersion = "0.0.1";
         
@@ -21,10 +21,78 @@ namespace PortalIndicator
         private void Awake()
         {
             // Jotunn comes with its own Logger class to provide a consistent Log style for all mods using it
-            Jotunn.Logger.LogInfo("ModStub has landed");
-            
-            // To learn more about Jotunn's features, go to
-            // https://valheim-modding.github.io/Jotunn/tutorials/overview.html
+            Jotunn.Logger.LogInfo("PortalIndicator has landed");
+
+            MinimapManager.OnVanillaMapAvailable += MinimapManager_OnVanillaMapDataLoaded;
+
+            Patches.Patcher.Patch();
+        }
+
+        private static void MinimapManager_OnVanillaMapDataLoaded()
+        {
+            Log.Info("MiniMapManager.OnVanillaMapDataLoaded");
+            // Ask the server to send us the portals
+            var myId = ZDOMan.GetSessionID();
+            var myName = Game.instance.GetPlayerProfile().GetName();
+            RPC.Client.RequestSync($"{myName} ({myId}) has joined the game");
+        }
+
+        public static void GameStarted()
+        {
+            Log.Info("Registering RPC handlers");
+            RPC.RPCManager.Register();
+        }
+
+        public static void UpdateFromPackage(ZPackage pkg)
+        {
+            var portalOverlay = MinimapManager.Instance.GetMapOverlay("PortalOverlay");
+            Log.Info("Here, we should update the client's map with all portal pins");
+            var count = pkg.ReadInt();
+
+
+            Log.Info($"Received {count} portals from server");
+
+            if (count > 0)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    var portalPkg = pkg.ReadPackage();
+                    var tag = portalPkg.ReadString();
+                    var location = portalPkg.ReadVector3();
+                    Log.Info($"Client writing {tag} to {location}");
+
+                    Minimap.instance.AddPin(location, Minimap.PinType.Icon4, tag, true, false);
+                }
+            }
+        }
+
+        public static void ProcessSyncRequest()
+        {
+            Log.Info("Responding to sync request");
+            RPC.Server.ResponseToSyncRequest(Package());
+        }
+
+        public static ZPackage Package()
+        {
+            var portals = ZDOMan.instance.GetPortals();
+
+            var pkg = new ZPackage();
+            pkg.Write(portals.Count);
+
+            foreach (var portal in portals)
+            {
+                pkg.Write(PackagePortal(portal));
+            }
+
+            return pkg;
+        }
+
+        public static ZPackage PackagePortal(ZDO portal)
+        {
+            var pkg = new ZPackage();
+            pkg.Write(portal.GetString("tag"));
+            pkg.Write(portal.GetPosition());
+            return pkg;
         }
     }
 }
